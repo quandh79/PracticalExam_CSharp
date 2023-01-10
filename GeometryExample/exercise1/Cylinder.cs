83a14303a4209e6f65c5314e760380b27063cfb7
quandh79 <dohongquantc@gmail.com>
2023-01-10 18:54:02 +0700

    1

---
+++ GeometryExample/exercise1/Cylinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample.exercise1
+{
+    public class Cylinder
+    {
+        public double radius { get; set; }
+        public double height { get; set; }
+
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public void Process()
+        {
+            BaseArea = Math.PI * radius * radius;
+            LateralArea = 2 * Math.PI * radius * height;
+            TotalArea = 2 * Math.PI * radius * (height + radius);
+            Volume = Math.PI * radius * radius * height;
+        }
+
+        public double BaseArea { get; private set; }
+        public double LateralArea { get; private set; }
+        public double TotalArea { get; private set; }
+        public double Volume { get; private set; }
+        public string Result()
+        {
+            Console.WriteLine("radius: " + radius);
+            Console.WriteLine("height: " + height);
+            return "Base Area: " + Math.Round(BaseArea, 2) + "\n" +
+                    "Lateral Area: " + Math.Round(LateralArea, 2) + "\n" +
+                    "Total Area: " + Math.Round(TotalArea, 2) + "\n" +
+                    "Volume: " + Math.Round(Volume, 2);
+        }
+    }
+}
---
+++ GeometryExample/exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using GeometryExample.exercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cylinder = new Cylinder(38.64, 22.48);
+        cylinder.Process();
+        Console.WriteLine(cylinder.Result());
+       
+    }
+}
